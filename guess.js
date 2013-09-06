4dc8da404a5c3ffc9d1be1cda121ba983eba4e74
Alec Geatches <throwpi@me.com>
2013-09-05 20:11:48 -0700

    Stylized code to make it more conventional

--- guess.js
+++ guess.js
@@ -1,53 +1,51 @@
-var gender = function(name) {
-	name = name.toLowerCase();
+exports.gender = function(fullName) {
+  var firstName = getFirstNameFromFullName(fullName);
+  firstName = firstName.toLowerCase();
 
-	var firstName;
-    var commaIndex = name.indexOf(",");
-    if (commaIndex >= 0) {
-    	firstName = name.split(/,(.+)/)[1].trim()
-    } else {
-    	firstName = name
-    }
-    firstName = firstName.split(/\s/)[0]
+  var freqMale = frequencyInFile(firstName, 'guess/male.txt');
+  var freqFemale = frequencyInFile(firstName, 'guess/female.txt');
 
-	var freqMale = frequencyInFile(firstName, "guess/male.txt");
-	var freqFemale = frequencyInFile(firstName, "guess/female.txt");
-
-	var pMale = freqMale / (freqMale + freqFemale);
-	var gender, confidence;
-	if (freqMale > freqFemale) {
-		gender = "male";
-		confidence = pMale;
-	} else if (freqFemale > freqMale) {
-		gender = "female";
-		confidence = 1 - pMale;
-	} else {
-		gender = "unknown";
-		confidence = null;
-	}
-    return {"gender": gender, "confidence": confidence};
+  var pMale = freqMale / (freqMale + freqFemale);
+  var gender, confidence;
+  if (freqMale > freqFemale) {
+    gender = 'male';
+    confidence = pMale;
+  } else if (freqFemale > freqMale) {
+    gender = 'female';
+    confidence = 1 - pMale;
+  } else {
+    gender = 'unknown';
+    confidence = null;
+  }
+  return {gender: gender, confidence: confidence};
 }
 
 function frequencyInFile(firstName, file) {
-	frequencies = parseFile(file);
-	if (frequencies[firstName] == undefined) {
-		return 0.0005;
-	} else {
-		return frequencies[firstName];
-	}
+  frequencies = parseFile(file);
+  if (frequencies[firstName] == undefined) {
+    return 0.0005;
+  } else {
+    return frequencies[firstName];
+  }
 }
 
 function parseFile(file) {
-	frequencies = {};
-	fs = require('fs');
-	var array = fs.readFileSync(file).toString().split("\n");
-	for(i in array) {
-		var parts = array[i].split(/\s+/);
-		var name = parts[0];
-		var frequency = parts[1];
-	    frequencies[name.toLowerCase()] = parseFloat(frequency);
-	}
-	return frequencies;
+  frequencies = {};
+  fs = require('fs');
+  var array = fs.readFileSync(file).toString().split('\n');
+  for(i in array) {
+    var parts = array[i].split(/\s+/);
+    var name = parts[0];
+    var frequency = parts[1];
+    frequencies[name.toLowerCase()] = parseFloat(frequency);
+  }
+  return frequencies;
 }
 
-exports.gender = gender;
+function getFirstNameFromFullName(fullName) {
+  var commaIndex = fullName.indexOf(',');
+  if (commaIndex >= 0) {
+    fullName = fullName.split(/,(.+)/)[1].trim();
+  }
+  return fullName.split(/\s/)[0];
+}
