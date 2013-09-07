1e5461abe63aca7c8d93dad47f87b4438eafc1ad
Alec Geatches <throwpi@me.com>
2013-09-06 22:22:09 -0700

    Improved benchmarking to benchmark real data and use many cycles

--- benchmark/bench.js
+++ benchmark/bench.js
@@ -1,14 +1,15 @@
 var gender = require('./../gender');
 var names = getNames();
 
-var cycles = 1;
-console.time('names benchmark');
-for (var i = 0; i < 2; i++) {
-  for (name in names) {
-    gender.guess(name)
-  }
+var cycles = 1000;
+var timerName = 'names benchmark (' + cycles + ' cycles)';
+console.time(timerName);
+for (var i = 0; i < cycles; i++) {
+  names.forEach(function (name) {
+    gender.guess(name);
+  });
 }
-console.timeEnd('names benchmark');
+console.timeEnd(timerName);
 
 function getNames() {
 	var fs = require('fs');
